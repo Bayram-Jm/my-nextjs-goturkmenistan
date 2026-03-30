28a07864eb1c8617e77a952deb9665f096604d10
Bayram-Jm <bjumayew@gmail.com>
2026-03-30 20:40:18 +0500

    fix: correct component order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import AppUse from "@/components/AppUse";
 import Ashgabat from "@/components/Ashgabat";
-import BookTicket from "@/components/BookTicket";
-import Cuisine from "@/components/Cuisine";
 import Events from "@/components/Events";
-import Footer from "@/components/Footer";
 import Heritage from "@/components/Heritage";
-import Nature from "@/components/Nature";
-import PayWithEase from "@/components/PayWithEase";
 import Tours from "@/components/Tours";
+import Nature from "@/components/Nature";
+import Cuisine from "@/components/Cuisine";
+import PayWithEase from "@/components/PayWithEase";
+import BookTicket from "@/components/BookTicket";
+import AppUse from "@/components/AppUse";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
-      <AppUse />
-      <Tours />
       <Ashgabat />
+      <Events />
       <Heritage />
+      <Tours />
       <Nature />
       <Cuisine />
-      <Events />
       <PayWithEase />
       <BookTicket />
+      <AppUse />
       <Footer />
     </main>
   );
