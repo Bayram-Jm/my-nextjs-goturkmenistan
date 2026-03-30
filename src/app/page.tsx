1795e7b130b30d1ad3054db9715aa834835dad07
Bayram-Jm <bjumayew@gmail.com>
2026-03-30 20:12:11 +0500

    add src directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold">Привет! Это мой сайт 🚀</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>Привет! Это мой сайт 🚀</h1>
     </div>
   );
 }
