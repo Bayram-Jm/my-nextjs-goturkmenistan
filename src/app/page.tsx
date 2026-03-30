53b74854bdd9ec7daeafcc1015bcc10ac573c10c
Bayram-Jm <bjumayew@gmail.com>
2026-03-30 20:24:36 +0500

    fix: add all components to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import AppUse from "@/components/AppUse";
+import Ashgabat from "@/components/Ashgabat";
+import BookTicket from "@/components/BookTicket";
+import Cuisine from "@/components/Cuisine";
+import Events from "@/components/Events";
+import Footer from "@/components/Footer";
+import Heritage from "@/components/Heritage";
+import Nature from "@/components/Nature";
+import PayWithEase from "@/components/PayWithEase";
+import Tours from "@/components/Tours";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1>Привет! Это мой сайт 🚀</h1>
-    </div>
+    <main>
+      <Header />
+      <Hero />
+      <AppUse />
+      <Tours />
+      <Ashgabat />
+      <Heritage />
+      <Nature />
+      <Cuisine />
+      <Events />
+      <PayWithEase />
+      <BookTicket />
+      <Footer />
+    </main>
   );
 }
